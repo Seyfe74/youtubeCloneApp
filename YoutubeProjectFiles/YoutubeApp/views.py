eddcc8b9d6b4e580f79dbc8084d8c9577c586a69
Djaypm89 <djaypm89@gmail.com>
2021-09-15 18:10:11 -0400

    serializers complete, just needs postman.  start on page 13, register new apps url endpoints

--- YoutubeProjectFiles/YoutubeApp/views.py
+++ YoutubeProjectFiles/YoutubeApp/views.py
@@ -9,7 +9,27 @@ from rest_framework import status
 class CommentList(APIView):
     def get(self, request):
         comment = Comment.objects.all()
-        reply = Reply.objects.all()
         serializer = CommentSerializer(comment, many=True)
-        serializerReply = ReplySerializer(reply, many=True)
+        return Response(serializer.data)
 
+    def post(self, request):
+        serializer = CommentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+class ReplyList(APIView):
+    def get(self, request):
+        reply = Reply.objects.all()
+        serializerReply = ReplySerializer(reply, many=True)
+        return Response(serializerReply.data)
+
+    def post(self, request):
+        serializerReply = ReplySerializer(data=request.data)
+        if serializerReply.is_valid():
+            serializerReply.save()
+            return Response(serializerReply.data, status=status.HTTP_201_CREATED)
+        return Response(serializerReply.errors, status=status.HTTP_400_BAD_REQUEST)
